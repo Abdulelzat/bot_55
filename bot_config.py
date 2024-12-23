ff01cece44cec7fa3052d517755aac0d9af74981
Abdulelzat <sakutakodzi@gmail.com>
2024-12-23 17:05:25 +0600

    hw 2

--- bot_config.py
+++ bot_config.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher
 from dotenv import dotenv_values
-from database import Database
 
 token = dotenv_values(".env")["BOT_TOKEN"]
 bot = Bot(token=token)
--- main.py
+++ main.py
@@ -1,48 +1,18 @@
 import asyncio
 import logging
-import random
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-from aiogram.filters import Command
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN")
-
-if not TOKEN:
-    raise ValueError("Отсутствует токен бота. Убедитесь, что он указан в файле .env под ключом BOT_TOKEN.")
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-
-NAMES = ["Алексей", "Мария", "Дмитрий", "Екатерина", "Иван", "Ольга"]
-
-@dp.message(Command("start"))
-async def start_command_handler(message: Message):
-    user_name = message.from_user.first_name or "друг"
-    await message.answer(f"Привет, {user_name}! Добро пожаловать в бота!")
-
-@dp.message(Command("myinfo"))
-async def myinfo_command_handler(message: Message):
-    user_id = message.from_user.id
-    first_name = message.from_user.first_name or "Не указано"
-    username = message.from_user.username or "Не указано"
-    await message.answer(
-        f"Ваши данные:\n"
-        f"ID: {user_id}\n"
-        f"Имя: {first_name}\n"
-        f"Username: {username}"
-    )
-
-@dp.message(Command("random"))
-async def random_command_handler(message: Message):
-    random_name = random.choice(NAMES)
-    await message.answer(f"Случайное имя: {random_name}")
+from bot_config import dp, bot
+from handlers.other_massages import echo_router
+from handlers.start import start_router
+from handlers.myinfo import myinfo_router
+from handlers.random import randomfile
 
 async def main():
-    logging.basicConfig(level=logging.INFO)
+    dp.include_router(start_router)
+    dp.include_router(myinfo_router)
+    dp.include_router(randomfile)
+    dp.include_router(echo_router)
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
