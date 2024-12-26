eb16d77c16bc5ed84142b522dec4fc0f4dd46de4
Abdulelzat <sakutakodzi@gmail.com>
2024-12-26 19:30:10 +0600

    homework6

---
+++ handlers/dishes.py
@@ -0,0 +1,17 @@
+from aiogram import Router, F, types
+from bot_config import database
+from pprint import pprint
+
+menu_list_router = Router()
+
+@menu_list_router.callback_query(F.data == "menu_list")
+async def show_all_menu(callback: types.CallbackQuery):
+    menu_list = database.get_menu_list()
+    pprint(menu_list)
+
+    for menu in menu_list:
+        txt = (f"Name: {menu['food_name']}\n"
+               f"Price: {menu['price']}\n"
+               f"Description: {menu['description']}\n"
+               f"Category: {menu['category']}\n")
+        await callback.message.answer(txt)
